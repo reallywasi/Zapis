e6a01d35907736bcdb56744cd670972301cf1423
nirmalkr01 <singhnirmalkr5@gmail.com>
2025-01-03 19:26:39 +0530

    Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      {/* Header */}
+      <header className="header">
+        <div className="logo">Zapis</div>
+        <nav className="nav">
+          <a href="#about">About</a>
+          <a href="#features">Features</a>
+          <a href="#register">Register</a>
+        </nav>
       </header>
+
+      <Login />
+      {/* Footer */}
+      <footer className="footer">
+        <p>&copy; {new Date().getFullYear()} Zapis. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
